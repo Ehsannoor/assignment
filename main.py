a5b316a12ac9be72626ff7d70e1bb25fd6f3b282
Ehsannoor <71089536+Ehsannoor@users.noreply.github.com>
2020-10-04 19:04:26 +0200

    Throwing virtual dice

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-n = input("Enter a number: ")
-factorial = 1
-if int(n) >= 1:
-    for i in range(1, int(n) + 1):
-        factorial = factorial * i
-        print("%s! = %s" % (i,factorial ))
 
+
+
+import random
+
+n = input("Enter a number of dice to throw: ")
+i = 1
+y=0
+x=0.1
+while (i<=n):
+     y=random.randint(1,6)
+     if i==1:
+      print ("\nDie %d: %d" % (i, y))
+      i = i + 1
+      x = x + y
+      y = 0
+     else:
+      print ("Die %d: %d" % (i, y))
+      i=i+1
+      x = x + y
+      y=0
+print ("\nAverage: %1.1f" % (x/n))
 
 
